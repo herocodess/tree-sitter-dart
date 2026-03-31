44f65aa88ae3ced12b2403fab899e60d5c188e11
herocodess <momohhero2@gmail.com>
2026-03-31 03:13:52 +0100

    fix: add nodeTypeInfo export, codesign darwin prebuilds

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const binding = require('node-gyp-build')(__dirname)
 module.exports = binding
 module.exports.default = binding
 module.exports.name = binding.name || 'dart'
+try {
+  module.exports.nodeTypeInfo = require('./src/node-types.json')
+} catch (_) {}
