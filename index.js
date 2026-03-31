4d81dfc0faf66758c6dd068f66907cb44f58830d
herocodess <momohhero2@gmail.com>
2026-03-31 03:30:34 +0100

    fix: clean up index.js exports, remove redundant default

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 'use strict'
+
 const binding = require('node-gyp-build')(__dirname)
+
+// Export the full binding object so tree-sitter@0.22 can find:
+// - binding.language (the tagged External<TSLanguage>)
+// - binding.name ('dart')
+// - binding.nodeTypeInfo (node type array for initializeLanguageNodeClasses)
+// Exporting binding.default alone loses these properties and causes
+// initializeLanguageNodeClasses to crash on undefined.
 module.exports = binding
-module.exports.default = binding
-module.exports.name = binding.name || 'dart'
+
 try {
   module.exports.nodeTypeInfo = require('./src/node-types.json')
 } catch (_) {}
